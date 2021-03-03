80c4063bd749130654339c0f37597c28813ecc0e
royalfelep <73192682+royalfelep@users.noreply.github.com>
2021-03-03 18:36:00 -0300

    Update card_animation.js

--- pokemon_card/card_animation.js
+++ pokemon_card/card_animation.js
@@ -1,4 +1,4 @@
-// Elementos a serem animados
+// Adding elements to animate
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 const pokemon = document.querySelector('.pokemon img')
@@ -7,28 +7,28 @@ const pokeinfo = document.querySelector('.pokeinfo')
 const buttons = document.querySelector('.buttons')
 
 
-// Animação de movimento
+// Animating section
 container.addEventListener('mousemove', (e) => {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 20
     let yAxis = (window.innerHeight / 2 - e.pageY) / 20
     card.style.transform = `rotateX(${yAxis}deg) rotateY(${(-xAxis)}deg)`
 });
 
-// in
-container.addEventListener('mouseenter', (e) =>{
+// Mouse in
+container.addEventListener('mouseenter', (e) => {
     card.style.transition = 'none';
-    //popout
+    // Pop out
     pokemon.style.transform = 'translateZ(50px) rotateZ(-10deg)';
     poketitle.style.transform = 'translateZ(50px)';
     pokeinfo.style.transform = 'translateZ(50px)';
     buttons.style.transform = 'translateZ(50px)';
 })
 
-// out
+// Mouse out
 container.addEventListener('mouseleave', (e) => {
     card.style.transform = `rotateX(0deg) rotateY(0deg)`
     card.style.transition = 'all 0.5s ease';
-    //popback
+    // Pop back
     pokemon.style.transform = 'translateZ(0px) rotateZ(0deg)';
     poketitle.style.transform = 'translateZ(0px)';
     pokeinfo.style.transform = 'translateZ(0px)';
